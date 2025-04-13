3e38f020e43af066deb3d815a4e9d11dc570e097
erarushiti <er68645@ubt-uni.net>
2025-04-13 23:55:52 +0200

    created register session form

--- frontend/src/pages/SessionRegistration/[sessionId]/register.tsx
+++ frontend/src/pages/SessionRegistration/[sessionId]/register.tsx
@@ -1,7 +1,11 @@
-// pages/sessions/[sessionId]/register.tsx
+"use client";
 import React, { useState, FormEvent } from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
+import '../../../app/globals.css'
+import Header from '@/components/Header';
+import CustomImage from "@/assets/images/fitness.png";
+
 
 // Define the Registration interface
 export interface Registration {
@@ -54,57 +58,88 @@ const SessionRegister: React.FC = () => {
   };
 
   return (
-    <div style={{ maxWidth: '400px', margin: '0 auto', padding: '20px' }}>
-      <h1>Register for Session</h1>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      {success && <p style={{ color: 'green' }}>{success}</p>}
-      <form onSubmit={handleSubmit}>
-        <div style={{ marginBottom: '15px' }}>
+    <div>
+         <Header/>
+       <div className='bg-[#111] min-h-screen pt-[147px] '>
+      <h1 className='text-center text-3xl font-extrabold text-white mb-[50px]'>Register for Session</h1>
+     
+      <form className=' flex flex-col items-center' onSubmit={handleSubmit}>
+        <div className='flex flex-col w-1/3 mb-[20px]'>
           <label>
             Full Name:
-            <input
+            </label>
+            <input className=' p-[6px] mt-[5px] border-2 border-[#6f6d6d] rounded-[8px]'
               type="text"
               name="fullName"
               value={formData.fullName}
               onChange={handleChange}
               required
-              style={{ width: '100%', padding: '8px', marginTop: '5px' }}
             />
-          </label>
+         
         </div>
-        <div style={{ marginBottom: '15px' }}>
+        <div className='flex flex-col w-1/3 mb-[20px]'>
           <label>
             Email:
-            <input
+            </label>
+            <input className=' p-[6px] mt-[5px] border-2 border-[#6f6d6d] rounded-[8px]'
               type="email"
               name="email"
               value={formData.email}
               onChange={handleChange}
               required
-              style={{ width: '100%', padding: '8px', marginTop: '5px' }}
             />
-          </label>
+          
         </div>
-        <div style={{ marginBottom: '15px' }}>
+        <div  className='flex flex-col w-1/3 mb-[40px]'>
           <label>
             Phone (Optional):
-            <input
+            </label>
+            <input className=' p-[6px] mt-[5px] border-2 border-[#6f6d6d] rounded-[8px]'
               type="tel"
               name="phone"
               value={formData.phone}
               onChange={handleChange}
-              style={{ width: '100%', padding: '8px', marginTop: '5px' }}
             />
-          </label>
+         
         </div>
-        <button
+        <button className='bg-[#6f6d6d] py-[10px] px-[20px] rounded-[8px] font-bold mb-[50px] '
           type="submit"
-          style={{ padding: '10px 20px', background: '#007BFF', color: 'white', border: 'none', cursor: 'pointer' }}
+          
         >
           Submit Registration
         </button>
+        {error && <p className='text-red-600 text-center text-base'>{error}</p>}
+        {success && <p className='text-green-600 text-center text-base'>{success}</p>}
       </form>
     </div>
+    <footer className=" rounded-lg shadow-sm bg-black ">
+    <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+            <a href="#" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+            <img className="max-h-screen object-contain" src={CustomImage.src} alt="Fitness Image" />
+                <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">My Fitness</span>
+            </a>
+            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm sm:text-center text-[#EE7838]">© 2023 <a href="#`" className="hover:underline ">My Fitnes</a>. All Rights Reserved.</span>
+    </div>
+</footer>  
+    </div>
+   
   );
 };
 
